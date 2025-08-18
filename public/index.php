9f99509c8ddb0455df127c8f13f8d6c3b2457937
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-08-18 23:34:36 +0100

    Auto-save changes

--- public/index.php
+++ public/index.php
@@ -1,6 +1,19 @@
 <?php
-// Example simple protection
-if ($_GET['view'] === 'map') {
+require_once __DIR__ . '/../src/GeoFencer.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['lat'], $_POST['lng'])) {
+    $geo = new GeoFencer();
+    if ($geo->isWithinGeofence($_POST['lat'], $_POST['lng'])) {
+        header("Location: https://docs.google.com/forms/d/e/.../viewform"); 
+        exit;
+    } else {
+        include __DIR__ . '/../views/error.html';
+        exit;
+    }
+}
+
+// Admin map view
+if (isset($_GET['view']) && $_GET['view'] === 'map') {
     session_start();
     if (!isset($_SESSION['admin']) || $_SESSION['admin'] !== true) {
         http_response_code(403);
